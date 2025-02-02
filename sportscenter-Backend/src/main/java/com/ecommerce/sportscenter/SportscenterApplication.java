3781a2d3bc83c54e504c0a101a3c23c967952e4c
Jiejia-Shi <j279shi@uwaterloo.ca>
2025-02-01 20:14:17 -0500

    Back-end framework

---
+++ sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/SportscenterApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.sportscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportscenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportscenterApplication.class, args);
+        System.out.println("Project runs successfully");
+    }
+
+}
