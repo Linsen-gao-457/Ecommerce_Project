0d13c08b9c758ffe496a9be604403afe9fd78630
y49du <y49du@uwaterloo.ca>
2025-02-20 16:50:44 -0500

    initial entity : Type, Brand, Product class

---
+++ sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/entity/Brand.java
@@ -0,0 +1,27 @@
+package com.ecommerce.sportscenter.entity;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Brand")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private Integer id;
+    @Column(name="Name")
+    private String name;
+
+    @OneToMany(mappedBy = "brand", fetch = FetchType.LAZY)
+    private List<Product> products;
+}
