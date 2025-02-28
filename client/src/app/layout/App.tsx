748a726d7223856961de4af7cc0e182114fa9e84
pangyujia66 <y3pang@uwaterloo.ca>
2025-02-27 22:03:42 -0500

    product details page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -21,7 +21,7 @@ function App() {
     <CssBaseline/>
     <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
     <Container sx={{ paddingTop: "64px" }}>
-      <Catalog/>
+      <Outlet/>
     </Container>
     </ThemeProvider>
 
