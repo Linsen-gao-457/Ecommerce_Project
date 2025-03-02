8025606e0a6ef41f8e96bea0cb9740cb77e1a624
Jiejia-Shi <j279shi@uwaterloo.ca>
2025-03-02 18:31:56 -0500

    change Table names of entities. Fixes #3

--- sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/entity/Brand.java
+++ sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/entity/Brand.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name="brand")
+@Table(name="Brand")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/entity/Type.java
+++ sportscenter-Backend/src/main/java/com/ecommerce/sportscenter/entity/Type.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name="type")
+@Table(name="Type")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
