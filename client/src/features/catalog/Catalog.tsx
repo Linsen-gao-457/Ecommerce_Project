9f20058165ddccd36762dc4610b4984628368a1f
pangyujia66 <y3pang@uwaterloo.ca>
2025-03-01 20:56:23 -0500

    create error page

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react";
-import { Product } from "../../app/models/product";
-import ProductList from "./ProductList";
-
-export default function Catalog(){
-const [products, setProducts] = useState<Product[]>([]);
-      useEffect(()=>{
-        fetch('http://localhost:8081/api/products')
-        .then(response =>response.json())
-        .then(data=>setProducts(data.content));
-      },[]);
-      return (
-        <>
+      import { useState, useEffect } from "react";
+      import { Product } from "../../app/models/product";
+      import ProductList from "./ProductList";
+      import agent from "../../app/api/agent";
+      // import Spinner from "../../app/layout/Spinner";
+      
+      export default function Catalog(){
+        const [products, setProducts] = useState<Product[]>([]);
+        const [loading, setLoading] = useState(true);
+        
+      // useEffect(()=>{
+      //   fetch('http://localhost:8081/api/products')
+      //   .then(response =>response.json())
+      //   .then(data=>setProducts(data.content));
+      // },[]);
+        useEffect(()=>{
+          agent.Store.list()  
+          .then((products)=>setProducts(products.content))
+          .catch(error=>console.log(error))
+          .finally(()=>setLoading(false));
+        }, []);
+        if(!products) return <h3>Unable to load Products</h3>
+        // if(loading) return <Spinner message='Loading Products...'/>
+        return (
+          <>
             <ProductList products={products}/>
-        </>
-
-      )
-
-}
+          </>
+        )
+      }
